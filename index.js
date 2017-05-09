68bef8ce0dc6a0dec9f75f5c72564756e563d38f
Jinliang <hou80houzhu@vip.qq.com>
2017-05-09 10:52:49 +0800

    init

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var builter=require("./base/builter");
-var sourcePath=require("path").resolve(__dirname,"./svg/");
-var outputPath=require("path").resolve(__dirname,"./com/topolr/icon/base.js");
-builter.outputFile(sourcePath,outputPath);
+var iconmapping={
+    weather:"./icons/weather",
+    action:"./icons/action",
+    editor:"./icons/editor",
+    files:"./icons/files",
+    loading:"./icons/loading",
+    player:"./icons/player",
+    tm:"./icons/tm"
+};
+console.log("Start Build");
+for(var i in iconmapping){
+    console.log("  Build:"+i);
+    var name=i,path=require("path").resolve(__dirname,iconmapping[i]);
+    var output=require("path").resolve(__dirname,"./com/topolr/icon/"+name+".js");
+    builter.outputFile(path,output);
+}
+console.log("Build Done");
